c71570251a2e7882244a6749f17c5c88e005e9be
Jingtao Wang <7776499+jt-wang@users.noreply.github.com>
2023-08-30 02:23:01 +0900

    nextjs 13 API route works with react component

---
+++ app/api/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const res =await fetch('https://api.thecatapi.com/v1/images/search')
+    const json = await res.json()
+    return NextResponse.json(json[0]);
+}
